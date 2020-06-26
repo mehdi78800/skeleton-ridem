9c546a09ab6c9c211d21798ab351cd2f5744dd1e
mehdi <regragui.mehdii@gmail.com>
2020-06-26 14:47:07 +0200

    reglage de bugs

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
  
-require '../vendor/autoload.php';
+ require '../vendor/autoload.php';
 
-$kernel = new Ridem\Kernel();
-
-
-$router = $kernel->getRouter();
-$router->load(
-    include dirname(__DIR__) . '/app/routes.php'
-);
-$router->run($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+ $kernel = new Ridem\Kernel();
+ 
+ $kernel->router->load(include dirname(__DIR__) . '/app/routes.php');
+ 
+ $kernel->run($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
